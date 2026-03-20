9b395ffebddaac993529275ca565b47ca23bc14b
thedeagleoutplay-hue <thedeagleoutplay@gmail.com>
2026-03-20 11:46:36 -0400

    Initialize Express server with MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/inventory')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
